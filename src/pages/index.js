eb52d500ffdf1c8567fcf5fc766d201f3121c713
noumanmalik960 <noumanmalik960@gmail.com>
2021-03-22 00:32:18 +0500

    theme ui styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Container, Button, Heading, Flex } from 'theme-ui'
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button sx={{ marginTop: 2 }} onClick={() => { alert("clicked") }}>Log In</Button>
+      </Flex>
+    </Container >
+  )
 }
